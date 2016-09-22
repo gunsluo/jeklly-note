867d975171383dd0d254b6cb3649c247b8d02a74
jerrylou <gunsluo@gmail.com>
2016-09-22 18:52:42 +0800

    add search featrue
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- js/search.js
+++ js/search.js
@@ -1,90 +1,144 @@
 jQuery(document).ready(function($){
-	var resizing = false,
-		navigationWrapper = $('.cd-main-nav-wrapper'),
-		navigation = navigationWrapper.children('.cd-main-nav'),
-		searchForm = $('.cd-main-search'),
-		pageContent = $('.cd-main-content'),
-		searchTrigger = $('.cd-search-trigger'),
-		coverLayer = $('.cd-cover-layer'),
-		navigationTrigger = $('.cd-nav-trigger'),
-		mainHeader = $('.cd-main-header');
-	
-	function checkWindowWidth() {
-		var mq = window.getComputedStyle(mainHeader.get(0), '::before').getPropertyValue('content').replace(/"/g, '').replace(/'/g, "");
-		return mq;
-	}
+  var resizing = false,
+    navigationWrapper = $('.cd-main-nav-wrapper'),
+    navigation = navigationWrapper.children('.cd-main-nav'),
+    searchForm = $('.cd-main-search'),
+    pageContent = $('.cd-main-content'),
+    searchTrigger = $('.cd-search-trigger'),
+    coverLayer = $('.cd-cover-layer'),
+    navigationTrigger = $('.cd-nav-trigger'),
+    mainHeader = $('.cd-main-header'),
+    suggestions = $('.cd-search-suggestions'),
+    postDom = suggestions.find('.news > ul');
+  
+  function checkWindowWidth() {
+    var mq = window.getComputedStyle(mainHeader.get(0), '::before').getPropertyValue('content').replace(/"/g, '').replace(/'/g, "");
+    return mq;
+  }
 
-	function checkResize() {
-		if( !resizing ) {
-			resizing = true;
-			(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
-		}
-	}
+  function checkResize() {
+    if( !resizing ) {
+      resizing = true;
+      (!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
+    }
+  }
 
-	function moveNavigation(){
-  		var screenSize = checkWindowWidth();
-        if ( screenSize == 'desktop' && (navigationTrigger.siblings('.cd-main-search').length == 0) ) {
-        	//desktop screen - insert navigation and search form inside <header>
-        	searchForm.detach().insertBefore(navigationTrigger);
-			navigationWrapper.detach().insertBefore(searchForm).find('.cd-serch-wrapper').remove();
-		} else if( screenSize == 'mobile' && !(mainHeader.children('.cd-main-nav-wrapper').length == 0)) {
-			//mobile screen - move navigation and search form after .cd-main-content element
-			navigationWrapper.detach().insertAfter('.cd-main-content');
-			var newListItem = $('<li class="cd-serch-wrapper"></li>');
-			searchForm.detach().appendTo(newListItem);
-			newListItem.appendTo(navigation);
-		}
+  function moveNavigation(){
+    var screenSize = checkWindowWidth();
+    if ( screenSize == 'desktop' && (navigationTrigger.siblings('.cd-main-search').length == 0) ) {
+      //desktop screen - insert navigation and search form inside <header>
+      searchForm.detach().insertBefore(navigationTrigger);
+      navigationWrapper.detach().insertBefore(searchForm).find('.cd-serch-wrapper').remove();
+    } else if( screenSize == 'mobile' && !(mainHeader.children('.cd-main-nav-wrapper').length == 0)) {
+      alert(2)
+      //mobile screen - move navigation and search form after .cd-main-content element
+      navigationWrapper.detach().insertAfter('.cd-main-content');
+      var newListItem = $('<li class="cd-serch-wrapper"></li>');
+      searchForm.detach().appendTo(newListItem);
+      newListItem.appendTo(navigation);
+    }
 
-		resizing = false;
-	}
+    resizing = false;
+  }
 
-	function closeSearchForm() {
-		searchTrigger.removeClass('search-form-visible');
-		searchForm.removeClass('is-visible');
-		coverLayer.removeClass('search-form-visible');
-	}
+  function closeSearchForm() {
+    searchTrigger.removeClass('search-form-visible');
+    searchForm.removeClass('is-visible');
+    coverLayer.removeClass('search-form-visible');
+  }
 
-	//add the .no-pointerevents class to the <html> if browser doesn't support pointer-events property
-	( !Modernizr.testProp('pointerEvents') ) && $('html').addClass('no-pointerevents');
+  //add the .no-pointerevents class to the <html> if browser doesn't support pointer-events property
+  ( !Modernizr.testProp('pointerEvents') ) && $('html').addClass('no-pointerevents');
 
-	//move navigation and search form elements according to window width
-	moveNavigation();
-	$(window).on('resize', checkResize);
+  //move navigation and search form elements according to window width
+  moveNavigation();
+  $(window).on('resize', checkResize);
 
-	//mobile version - open/close navigation
-	navigationTrigger.on('click', function(event){
-		event.preventDefault();
-		mainHeader.add(navigation).add(pageContent).toggleClass('nav-is-visible');
-	});
+  //mobile version - open/close navigation
+  navigationTrigger.on('click', function(event){
+    event.preventDefault();
+    mainHeader.add(navigation).add(pageContent).toggleClass('nav-is-visible');
+  });
 
-	searchTrigger.on('click', function(event){
-		event.preventDefault();
-		if( searchTrigger.hasClass('search-form-visible') ) {
-			searchForm.find('form').submit();
-		} else {
-			searchTrigger.addClass('search-form-visible');
-			coverLayer.addClass('search-form-visible');
-			searchForm.addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-				searchForm.find('input[type="search"]').focus().end().off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-			});
-		}
-	});
+  searchTrigger.on('click', function(event){
+    event.preventDefault();
+    if( searchTrigger.hasClass('search-form-visible') ) {
+      searchPost();
+    } else {
+      searchTrigger.addClass('search-form-visible');
+      coverLayer.addClass('search-form-visible');
+      searchForm.addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+        searchForm.find('input[type="search"]').focus().end().off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+      });
+    }
+  });
 
-	//close search form
-	searchForm.on('click', '.close', function(){
-		closeSearchForm();
-	});
+  //close search form
+  searchForm.on('click', '.close', function(){
+    closeSearchForm();
+  });
 
-	coverLayer.on('click', function(){
-		closeSearchForm();
-	});
-	
-	$(document).keyup(function(event){
-		if( event.which=='27' ) closeSearchForm();
-	});
+  coverLayer.on('click', function(){
+    closeSearchForm();
+  });
 
-	//upadate span.selected-value text when user selects a new option
-	searchForm.on('change', 'select', function(){
-		searchForm.find('.selected-value').text($(this).children('option:selected').text());
-	});
+  // press enter
+  searchForm.keydown(function(event){
+    if( event.which=='13' ) {
+      //event.preventDefault();
+      return false;
+    }
+
+    if( event.which=='13' ) {
+      closeSearchForm();
+      return false;
+    }
+    console.log(event.which)
+
+    searchPost();
+    return true;
+  });
+
+  $(document).keyup(function(event){
+    if( event.which=='27' ) closeSearchForm();
+  });
+
+  //upadate span.selected-value text when user selects a new option
+  searchForm.on('change', 'select', function(){
+    searchForm.find('.selected-value').text($(this).children('option:selected').text());
+  });
+
+  // add by jerrylou
+  function searchPost() {
+    let content = searchForm.find('input').val();
+    if( content == "" ){
+      //closeSearchForm();
+      return;
+    }
+    searchPostByContent(content.toLowerCase().trim());
+  }
+  
+  function searchPostByContent(content) {
+    console.log(content)
+    if( typeof postMap == "undefined" ) {
+      return;
+    }
+
+    let innerhtml = '';
+    for(let key in postMap ) {
+      let category = postMap[key];
+      for(let idx in category){
+        let post = category[idx];
+        if( (post.title.toLowerCase().indexOf(content) >= 0) ||
+          (post.subtitle.toLowerCase().indexOf(content) >= 0) ) {
+          let li = '<li>' + 
+            '<a class="image-wrapper" href="#"><img src="img/placeholder.png" alt="News image"></a>' +
+            '<h4><a class="cd-nowrap" href="#">' + post.title + '</a></h4>' +
+            '<time datetime="">' + post.subtitle + '</time></li>';
+          innerhtml += li;
+        }
+      }
+    }
+    postDom.html(innerhtml);
+  }
 });
