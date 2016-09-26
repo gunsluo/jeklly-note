61336ab75b94c46f59529357af2b02322a6a8317
jerrylou <gunsluo@gmail.com>
2016-09-26 21:32:26 +0800

    images by jekyll-assets plugin
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- src/_assets/js/search.js
+++ src/_assets/js/search.js
@@ -131,7 +131,7 @@ jQuery(document).ready(function($){
         if( (post.title.toLowerCase().indexOf(content) >= 0) ||
           (post.subtitle.toLowerCase().indexOf(content) >= 0) ) {
           var li = '<li>' + 
-            '<a class="image-wrapper" href="' + post.url + '"><img src="/img/placeholder.png" alt="News image"></a>' +
+            '<a class="image-wrapper" href="' + post.url + '"><img src="/assets/images/placeholder.png" alt="News image"></a>' +
             '<h4><a class="cd-nowrap" href="' + post.url + '">' + post.title + '</a></h4>' +
             '<time datetime="">' + post.subtitle + '</time></li>';
           innerhtml += li;
@@ -160,7 +160,7 @@ jQuery(document).ready(function($){
     for(var idx in category){
       var post = category[idx];
       var li = '<li>' + 
-        '<a class="image-wrapper" href="' + post.url + '"><img src="/img/placeholder.png" alt="News image"></a>' +
+        '<a class="image-wrapper" href="' + post.url + '"><img src="/assets/images/placeholder.png" alt="News image"></a>' +
         '<h4><a class="cd-nowrap" href="' + post.url + '">' + post.title + '</a></h4>' +
         '<time datetime="">' + post.subtitle + '</time></li>';
       innerhtml += li;
